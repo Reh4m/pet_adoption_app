a3136acf6dddc20c9f8fe544826f9c158aff5d94
emmarp <ruiizemmanuel@gmail.com>
2025-05-31 19:22:54 -0600

    Feat: App Theme provider

---
+++ lib/src/presentation/providers/theme_provider.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:pet_adoption_app/src/presentation/config/themes/index.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  final SharedPreferences prefs;
+
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeProvider(this.prefs) {
+    _loadAppTheme();
+  }
+
+  ThemeMode get currentThemeMode => _themeMode;
+  ThemeData get lightTheme => AppTheme.lightTheme;
+  ThemeData get darkTheme => AppTheme.darkTheme;
+
+  Future<void> _loadAppTheme() async {
+    // Load current theme mode
+    String themeModeString = prefs.getString('themeMode') ?? 'light';
+
+    switch (themeModeString) {
+      case 'light':
+        _themeMode = ThemeMode.light;
+        break;
+      case 'dark':
+        _themeMode = ThemeMode.dark;
+        break;
+      default:
+        _themeMode = ThemeMode.system;
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode = mode;
+
+    await prefs.setString(
+      'themeMode',
+      mode == ThemeMode.light ? 'light' : 'dark',
+    );
+
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    if (_themeMode == ThemeMode.light) {
+      await setThemeMode(ThemeMode.dark);
+    } else {
+      await setThemeMode(ThemeMode.light);
+    }
+  }
+}
